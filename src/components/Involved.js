84e8925ed46e8e1cc0a84e5e872f22f64dfcaf33
zahin430 <zhprangon@mun.ca>
2019-06-26 04:35:02 -0230

    Added minor changes and added stuffs in todo

--- src/components/Involved.js
+++ src/components/Involved.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 
-const Involved = () => {
-    return (    
-    <div class="jumbotron jumbotron-fluid">
-    <div class="container">
-        <h1 class="display-4">Fluid jumbotron</h1>
-        <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
-    </div>
-    </div>
-    );
+class Involved extends Component  {
+    render() {
+        var text = "Hello"
+        return (    
+            <div class="jumbotron jumbotron-fluid">
+            <div class="container">
+                <h1 class="display-4">{text}</h1>
+                <p class="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+            </div>
+            </div>
+            );
+    }
+    
 }
 
 export default Involved;
