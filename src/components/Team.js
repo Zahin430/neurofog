c725f2583d140d51ec93634b32c516718c20e4d0
Zahin430 <zhprangon@mun.ca>
2019-07-03 17:26:33 -0230

    Doing the team page seems allrightz

--- src/components/Team.js
+++ src/components/Team.js
@@ -2,7 +2,9 @@ import React from 'react';
 import juliarose21 from './img/juliarose21.jpg';
 import jon from './img/jon.jpg';
 import ian from './img/iain2.jpg';
-import rachelle from './img/rachelle.PNG'
+import rachelle from './img/rachelle.PNG';
+import julia from './img/julia.jpg'
+import female from './img/Female.jpg'
 
 const Team = () => {
     return (
@@ -14,8 +16,8 @@ const Team = () => {
           <div className="row text-center text-md-left">
 
             <div className="col-xl-6 col-lg-12 mb-5 d-md-flex justify-content-between">
-              <div className="avatar mb-md-0 mb-4 mx-4 team">
-                <img src={jon} className="rounded z-depth-1" alt="avatar"/>
+              <div className="avatar mb-md-0 mb-4 mx-4 team" style = {{ width: "80%", height: "100%" }}>
+                <img src={jon} className="rounded z-depth-1" style = {{ width: "100%", height: "100%" }} alt="avatar"/>
               </div>
               <div className="mx-4">
                 <h4 className="font-weight-bold mb-3">Jonathan Fawcett</h4>
@@ -44,7 +46,7 @@ const Team = () => {
 
   
     <div className="col-xl-6 col-lg-12 mb-xl-0 mb-5 d-md-flex justify-content-between">
-      <div className="avatar mb-md-0 mx-4 team" style = {{ width: "100%", height: "100%" }}>
+      <div className="avatar mb-md-0 mx-4 team" style = {{ width: "85%", height: "100%" }}>
         <img src = {ian} className = "rounded z-depth-1" style = {{ width: "100%", height: "100%" }} alt="Sample avatar"/>
       </div>
       <div className="mx-4">
@@ -75,32 +77,61 @@ const Team = () => {
 
   
     <div className="col-xl-6 col-lg-12 mb-xl-0 mb-5 d-md-flex justify-content-between">
-    <div className="avatar mb-md-0 mb-4 mx-4">
-        <img src={jon} className="rounded z-depth-1" alt="Sample avatar"/>
+    <div className="avatar mb-md-0 mb-4 mx-4" style = {{ width: "100%", height: "100%" }}>
+        <img src={julia} className="rounded z-depth-1" style = {{ width: "100%", height: "100%" }} alt="Sample avatar"/>
     </div>
     <div className="mx-4">
-        <h4 className="font-weight-bold mb-3">Anna Deynah</h4>
-        <h6 className="font-weight-bold grey-text mb-3">Web Developer</h6>
-        <p className="grey-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod eos id officiis hic
-        tenetur.</p>
+        <h4 className="font-weight-bold mb-3">Julia Greenham</h4>
+        <h6 className="font-weight-bold grey-text mb-3">Volunteer</h6>
+        <p className="grey-text">Julia has been involved with the Neurofog lab since May, 2017 when she held an NSERC USRA under the supervision of Dr. Fawcett. She has since taken the role as laboratory manager.</p>
     </div>
     </div>
 
 
 
     <div className="col-xl-6 col-lg-12 d-md-flex justify-content-between">
-    <div className="avatar mb-md-0 mb-4 mx-4">
-        <img src="https://mdbootstrap.com/img/Photos/Avatars/img%20(29).jpg" className="rounded z-depth-1 img-fluid"
+    <div className="avatar mb-md-0 mb-4 mx-4" style = {{ width: "40%", height: "100%" }}>
+        <img src={female} className="rounded z-depth-1 img-fluid" style = {{ width: "100%", height: "100%" }}
         alt="Sample avatar"/>
     </div>
     <div className="mx-4">
-        <h4 className="font-weight-bold mb-3">Sarah Melyah</h4>
-        <h6 className="font-weight-bold grey-text mb-3">Front-end Developer</h6>
-        <p className="grey-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod eos id officiis hic
-        tenetur.</p>
+        <h4 className="font-weight-bold mb-3">Emily Joy Buchanan</h4>
+        <h6 className="font-weight-bold grey-text mb-3">Volunteer</h6>
+        <p className="grey-text">Emily is a new volunteer in the Neuro Fog laboratory.</p>
     </div>
     </div>
     </div>
+
+
+    <div className="row text-center text-md-left my-5">
+
+  
+<div className="col-xl-6 col-lg-12 mb-xl-0 mb-5 d-md-flex justify-content-between">
+<div className="avatar mb-md-0 mb-4 mx-4" style = {{ width: "100%", height: "100%" }}>
+    <img src={julia} className="rounded z-depth-1" style = {{ width: "100%", height: "100%" }} alt="Sample avatar"/>
+</div>
+<div className="mx-4">
+    <h4 className="font-weight-bold mb-3">Julia Greenham</h4>
+    <h6 className="font-weight-bold grey-text mb-3">Volunteer</h6>
+    <p className="grey-text">Julia has been involved with the Neurofog lab since May, 2017 when she held an NSERC USRA under the supervision of Dr. Fawcett. She has since taken the role as laboratory manager.</p>
+</div>
+</div>
+
+
+
+<div className="col-xl-6 col-lg-12 d-md-flex justify-content-between">
+<div className="avatar mb-md-0 mb-4 mx-4" style = {{ width: "40%", height: "100%" }}>
+    <img src={female} className="rounded z-depth-1 img-fluid" style = {{ width: "100%", height: "100%" }}
+    alt="Sample avatar"/>
+</div>
+<div className="mx-4">
+    <h4 className="font-weight-bold mb-3">Emily Joy Buchanan</h4>
+    <h6 className="font-weight-bold grey-text mb-3">Volunteer</h6>
+    <p className="grey-text">Emily is a new volunteer in the Neuro Fog laboratory.</p>
+</div>
+</div>
+</div>
+
 
  
         
