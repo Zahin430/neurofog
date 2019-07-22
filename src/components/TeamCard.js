8ed12c0c890ed0ab599d163923cb19ebd2ea70b3
Zahin430 <zhprangon@mun.ca>
2019-07-22 16:46:07 -0230

    Added team props

--- src/components/TeamCard.js
+++ src/components/TeamCard.js
@@ -10,68 +10,123 @@ export default class TeamCard extends Component {
                     "name": "Jonathan Fawcett",
                     "role": "Lab Director | Assistant Professor",
                     "description": "Dr. Fawcett is the owner and oporator of the Neuro Fog laboratory. He is currently an Assisant Professor at MUN and has been for the last 3 years.",
-                    "image": 'juliarose21'
+                    "image": 'jon',                  
                 },
                 {
-                    "name": "Jonathan Fawcett",
-                    "role": "Lab Director | Assistant Professor",
+                    "name": "Rachelle Wakeham-Lewis",
+                    "role": "Lab Manager | Honours Student",
                     "description": "Dr. Fawcett is the owner and oporator of the Neuro Fog laboratory. He is currently an Assisant Professor at MUN and has been for the last 3 years.",
-                    "image": 'jon'
+                    "image": 'rachelle'
                 },
                 {
-                    "name": "Jonathan Fawcett",
-                    "role": "Lab Director | Assistant Professor",
-                    "description": "Dr. Fawcett is the owner and oporator of the Neuro Fog laboratory. He is currently an Assisant Professor at MUN and has been for the last 3 years.",
-                    "image":'jon'
+                    "name": "Julia Rose",
+                    "role": "NSERC Student",
+                    "description": "Julia is currently completing her BSc (Hons) in Psychology. She is working with Dr. Fawcett and Dr. Rash to determine the cognitive mechanisms involved in managing past and future painful events.",
+                    "image":'juliarose21',
+
                 },
                 {
-                    "name": "Jonathan Fawcett",
-                    "role": "Lab Director | Assistant Professor",
-                    "description": "Dr. Fawcett is the owner and oporator of the Neuro Fog laboratory. He is currently an Assisant Professor at MUN and has been for the last 3 years.",
-                    "image": 'jon'
+                    "name": "Iain Gamba",
+                    "role": "NSERC Student",
+                    "description": "Iain is completing his BSc (Hons) in Behavioural Neuroscience with hops to persue Medicine. He is currently working on his Honours project with Dr. Fawcett.",
+                    "image": 'iainM'
                 },
                 {
-                    "name": "Jonathan Fawcett",
-                    "role": "Lab Director | Assistant Professor",
+                    "name": "Janaya Toope ",
+                    "role": "Honours Student ",
                     "description": "Dr. Fawcett is the owner and oporator of the Neuro Fog laboratory. He is currently an Assisant Professor at MUN and has been for the last 3 years.",
-                    "image": 'jon'
+                    "image": 'jon',
                 },
+                {
+                    "name": "Chelsea Lahey",
+                    "role": "Honours Student ",
+                    "description": "Dr. Fawcett is the owner and oporator of the Neuro Fog laboratory. He is currently an Assisant Professor at MUN and has been for the last 3 years.",
+                    "image": 'jon',
+                },
+                {
+                    "name": "Hannah Willoughby ",
+                    "role": "Masters",
+                    "description": "Iain is completing his BSc (Hons) in Behavioural Neuroscience with hops to persue Medicine. He is currently working on his Honours project with Dr. Fawcett.",
+                    "image": 'iainM'
+                },
+                {
+                    "name": "Kelsi Hall",
+                    "role": "Masters",
+                    "description": "Dr. Fawcett is the owner and oporator of the Neuro Fog laboratory. He is currently an Assisant Professor at MUN and has been for the last 3 years.",
+                    "image": 'jon',
+                },
+                {
+                    "name": "Vanessa Strong",
+                    "role": "Volunteer",
+                    "description": "Iain is completing his BSc (Hons) in Behavioural Neuroscience with hops to persue Medicine. He is currently working on his Honours project with Dr. Fawcett.",
+                    "image": 'iainM'
+                },
+                {
+                    "name": "Kathleen Prior",
+                    "role": "Volunteer",
+                    "description": "Iain is completing his BSc (Hons) in Behavioural Neuroscience with hops to persue Medicine. He is currently working on his Honours project with Dr. Fawcett.",
+                    "image": 'iainM'
+                },
+                {
+                    "name": "Michael Foote ",
+                    "role": "Volunteer",
+                    "description": "Iain is completing his BSc (Hons) in Behavioural Neuroscience with hops to persue Medicine. He is currently working on his Honours project with Dr. Fawcett.",
+                    "image": 'iainM'
+                },
+                {
+                    "name": "Cierra Okeefe",
+                    "role": "Volunteer",
+                    "description": "Iain is completing his BSc (Hons) in Behavioural Neuroscience with hops to persue Medicine. He is currently working on his Honours project with Dr. Fawcett.",
+                    "image": 'iainM'
+                },
+                {
+                    "name": "Zahin Hasan Prangon",
+                    "role": "MUCEP Student",
+                    "description": "Dr. Fawcett is the owner and oporator of the Neuro Fog laboratory. He is currently an Assisant Professor at MUN and has been for the last 3 years.",
+                    "image": 'jon',
+                },
+                {
+                    "name": "Zahin Hasan Prangon",
+                    "role": "MUCEP Student",
+                    "description": "Dr. Fawcett is the owner and oporator of the Neuro Fog laboratory. He is currently an Assisant Professor at MUN and has been for the last 3 years.",
+                    "image": 'jon',
+                },
+                
                 
             ]
         }
 
     return (
-        <section className="team-section my-5">
-            <h2 className="h1-responsive text-center my-5" style = {{ letterSpacing: '2px' }}>NEURO FOG LABORATORY</h2>
-            <p className="grey-text text-center w-responsive mx-auto mb-5">Current members of the labratory.</p> 
+        <div>
 
-            <div className= "row text-center text-md-left">
-            {
-                teamData.members && teamData.members.map((item) => {
-                    return (
-                       
-                            <div className="col-xl-6 col-lg-12 mb-5 d-md-flex justify-content-between">
-                                <div className="avatar mb-md-0 mb-4 mx-4 team" style = {{ width: "80%", height: "100%" }}>
-                                    <img  src={require(`./img/${item.image}.jpg`)} className="rounded z-depth-1" style = {{ width: "100%", height: "100%" }} alt="avatar"/>
-                                </div>
-                    
-                                <div className="mx-4">
-                                    <h4 className="font-weight-bold mb-3">{item.name}</h4>
-                                    <h6 className="font-weight-bold grey-text mb-3">{item.role}</h6>
-                                    <p className="grey-text">{item.description}</p>
-                                </div>
-                            </div>
-                        
-                    )
-                })
-                  
-            }
-            </div>
-            
-          
+        <section className="team-section my-5 mb-100">
+                    <h2 className="h1-responsive text-center my-5" style = {{ letterSpacing: '2px' }}>NEURO FOG LABORATORY</h2>
+                    <p className="grey-text text-center w-responsive mx-auto mb-5">Current members of the labratory.</p> 
 
-                
-        </section>
+                    <div className= "row text-center text-md-left">
+                    {
+                        teamData.members && teamData.members.map((item) => {
+                            return (
+                                    <div className="col-xl-6 col-lg-12 mb-5 d-md-flex justify-content-between">
+                                        <div className="avatar mb-md-0 mb-4 mx-4 team" style = {{ width: '60%' }}>
+                                            <img  src={require(`./img/${item.image}.jpg`)} className="rounded z-depth-1" style = {{ width: 'auto', maxWidth: '100%', height: 'auto', maxHeight: '100%' }} alt="avatar"/>
+                                        </div>
+                            
+                                        <div className="mx-6">
+                                            <h4 className="font-weight-bold mb-3">{item.name}</h4>
+                                            <h6 className="font-weight-bold grey-text mb-3">{item.role}</h6>
+                                            <p className="grey-text">{item.description}</p>
+                                        </div>
+                                    </div>
+                                
+                            )
+                        })
+                    }
+                    </div>
+                </section>
+
+        </div>
+        
     );
     }
 }
