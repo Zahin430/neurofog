afaa3872bb30015a6f1e2d7084c7e2d58f72e15c
Zahin430 <zhprangon@mun.ca>
2019-07-24 00:52:47 -0230

    Added stylingszzz

--- src/components/SecondLanding.js
+++ src/components/SecondLanding.js
@@ -7,7 +7,7 @@ import participation from './img/participation_2.jpeg'
 const SecondLanding = () => {
     return (
     <div className="card-deck" style= {{padding: '5%' , width: '100%', height: '100%'}}>
-    <div className="card shadow bg-white rounded" >
+    <div className="card shadow bg-white rounded" id ="card1">
     <img className="card-img-top" src="http://neurofog.ca/img/teamsmall.jpg" alt="Card1"/>
     <div className="card-body">
       <h5 className="card-title">
@@ -16,7 +16,7 @@ const SecondLanding = () => {
       <p className="card-text">View our current and past members of the Neuro Fog Laboratory.</p>
     </div>
   </div>
-  <div className="card shadow bg-white rounded">
+  <div className="card shadow bg-white rounded" id ="card2">
     <img className="card-img-top" style= {{ height: '67%', width: '100%' }} src={researchMain} alt="Card2"/>
     <div className="card-body">
       <h5 className="card-title">
@@ -25,7 +25,7 @@ const SecondLanding = () => {
       <p className="card-text">Take a look at what research goes on in our laboratory.</p>
     </div>
   </div>
-  <div className="card shadow bg-white rounded">
+  <div className="card shadow bg-white rounded" id ="card3">
     <img className="card-img-top" src={participation}  style= {{ height: '67%'}} alt="Card3"/>
     <div className="card-body">
       <h5>
