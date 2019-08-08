7d2f9b60bf82db73e6bc93a72217a7d68ed7c2db
Zahin430 <zhprangon@mun.ca>
2019-08-08 15:47:55 -0230

    added route for experiment

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Research from './components/Research';
 import Involved from './components/Involved';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import experiment from './components/Experiment'
 
 import teamData from './components/teamData';
 
@@ -33,9 +34,12 @@ class App extends Component{
             render = { () => <Team teamData = {teamData} />} />
             <Route exact path= '/research' component={ Research } />
             <Route exact path= '/involved' component={ Involved } />
+            <Route exact path= '/experiment' component={ experiment } />
+
             <Route exact path= '/contact'
               render={ () => <Contact env = {this.props.env} /> } />
-            
+            {/* <Route exact path = "/visualltm.html"
+            render {() => {window.location.href = "visualltm.html"}} /> */}
           </Switch> 
 
           <Footer />
