b079a55e1e6ee4eaaa5617f6154f8fd874170ba3
Zahin430 <zhprangon@mun.ca>
2019-08-27 20:34:13 -0230

    Added a redirect route and changed team page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Fragment, Component } from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route, Switch, } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import Navbar from './components/Navbar';
 import Landing from './components/Landing';
-import Team from './components/TeamCard';
+import Team from './components/Team';
 import Research from './components/Research';
 import Involved from './components/Involved';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import experiment from './components/Experiment'
-
-import teamData from './components/teamData';
 import Alumni from './components/Alumni';
 
 class App extends Component{
@@ -30,16 +28,20 @@ class App extends Component{
 
           <Switch>
       
-            <Route exact path= '/team' 
-            render = { () => <Team teamData = {teamData} />} />
+          {/*  <Route exact path= '/team' 
+              render = { () => <Team teamData = {teamData} />} /> */}
+
+            <Route exact path= '/team' component={ Team } />  
             <Route exact path= '/research' component={ Research } />
             <Route exact path= '/involved' component={ Involved } />
             <Route exact path= '/experiment' component={ experiment } />
             <Route exact path = '/alumni' component = {Alumni} />
             <Route exact path= '/contact'
               render={ () => <Contact env = {this.props.env} /> } />
+            <Route render={() => <Redirect to="/" />} />          
+            
           </Switch> 
-
+            
           <Footer />
     
       </section>
