11c55ef7265fdcf4561cbe824d1244d1ca83968b
Zahin430 <zhprangon@mun.ca>
2019-08-27 20:37:55 -0230

    teamData prop array

--- src/components/teamData.js
+++ src/components/teamData.js
@@ -1,12 +1,168 @@
 let teamData = {
-    "members" : [
+    "MSC": [
         {
-            "name": "Jonathan Fawcett",
-            "role": "Lab Director | Assistant Professor",
-            "description": "Dr. Fawcett is the owner and oporator of the Neuro Fog laboratory. He is currently an Assisant Professor at MUN and has been for the last 3 years."
+            "name": "Chris Clarke",
+            "role": "MSc Student",
+            "description": "Duis aliquip id elit sunt eu sit consectetur sit ex incididunt excepteur. Incididunt quis aute pariatur consequat veniam consectetur et nisi fugiat. Fugiat excepteur fugiat id cillum in ut. Officia incididunt aliquip anim est minim ipsum. Pariatur nulla pariatur minim esse enim ea irure. Eu sint do eu incididunt exercitation consequat fugiat. Amet reprehenderit adipisicing ut do.",
+            "image": 'avatarMen',
+            width: "65%",
+            width2: "100%",
+            width3: "100%",
+            height: '100%'
 
+        },
+        {
+            "name": "Dalainey Drakes",
+            "role": "MSc Student (Co-supervised w/ Dr. Emily Fawcett)",
+            "description": "Duis aliquip id elit sunt eu sit consectetur sit ex incididunt excepteur. Incididunt quis aute pariatur consequat veniam consectetur et nisi fugiat. Fugiat excepteur fugiat id cillum in ut. Officia incididunt aliquip anim est minim ipsum. Pariatur nulla pariatur minim esse enim ea irure. Eu sint do eu incididunt exercitation consequat fugiat. Amet reprehenderit adipisicing ut do.",
+            "image": 'avatarMen',
+            width: "65%",
+            width2: "100%",
+            width3: "100%",
+            height: '100%'
+
+        },
+        {
+            "name": "Kelsi Hall",
+            "role": "MSc Student",
+            "description": "Duis aliquip id elit sunt eu sit consectetur sit ex incididunt excepteur. Incididunt quis aute pariatur consequat veniam consectetur et nisi fugiat. Fugiat excepteur fugiat id cillum in ut. Officia incididunt aliquip anim est minim ipsum. Pariatur nulla pariatur minim esse enim ea irure. Eu sint do eu incididunt exercitation consequat fugiat. Amet reprehenderit adipisicing ut do.",
+            "image": 'kelsi',
+            width: "65%",
+            width2: "100%",
+            width3: "100%",
+            height: '100%'
+
+        },
+        {
+            "name": "Julia Rose",
+            "role": "MSc Student (Co-supervised w/ Dr. Joshua Rash)",
+            "description": "Duis aliquip id elit sunt eu sit consectetur sit ex incididunt excepteur. Incididunt quis aute pariatur consequat veniam consectetur et nisi fugiat. Fugiat excepteur fugiat id cillum in ut. Officia incididunt aliquip anim est minim ipsum. Pariatur nulla pariatur minim esse enim ea irure. Eu sint do eu incididunt exercitation consequat fugiat. Amet reprehenderit adipisicing ut do.",
+            "image": 'avatarMen',
+            width: "65%",
+            width2: "100%",
+            width3: "100%",
+            height: '100%'
+
+        },
+        {
+            "name": "Hannah Willoughby ",
+            "role": "Masters",
+            "description": "Hannah is a Master's student and has been involved in various studies examining recognition and recall memory. Currently, she is interested in using EEG to further her understanding of the cognitive processes underlying our memory. ",
+            "image": 'hannah',
+            width: "100%",
+            width2: "100%",
+            width3: "100%",
+            height: '100%'
+
+        },
+
+    ],
+    "Honors": [
+        {
+            "name": "Michael Foote ",
+            "role": "Honors Student (Co-supervised w/ Dr. Michelle Ploughman)",
+            "description": "Julia is currently completing her BSc (Hons) in Psychology. She is working with Dr. Fawcett and Dr. Rash to determine the cognitive mechanisms involved in managing past and future painful events.",
+            "image": 'juliarose21',
+            width: "65%",
+            width2: "100%",
+            width3: "100%",
+            height: '100%'
+
+        },
+        {
+            "name": "Janaya Toope ",
+            "role": "Honours Student ",
+            "description": "Member of the Laboratory.",
+            "image": 'avatarMen',
+            width: "65%",
+            width2: "100%",
+            width3: "100%",
+            height: '100%'
+        },
+        {
+            "name": "Chelsea Lahey",
+            "role": "Honours Student ",
+            "description": "Member of the Laboratory.",
+            "image": 'chelsea',
+            width: "100%",
+            width2: "100%",
+            width3: "100%",
+            height: '100%'
+        },
+        {
+            "name": "Cayley Thoms",
+            "role": "Honours Student ",
+            "description": "Member of the Laboratory.",
+            "image": 'chelsea',
+            width: "100%",
+            width2: "100%",
+            width3: "100%",
+            height: '100%'
+        },
+        
+
+
+    ],
+
+    "Volunteers": [
+
+        {
+            "name": "Aime Hirwa",
+            "role": "Volunteer",
+            "description": "Kathleen is a new volunteer in the Neuro Fog laboratory.",
+            "image": 'avatarMen',
+            width: "65%",
+            width2: "100%",
+            width3: "100%",
+            height: '100%'
+        },
+        {
+            "name": "Cierra O'Keefe",
+            "role": "Volunteer",
+            "description": "Iain is completing his BSc (Hons) in Behavioural Neuroscience with hopes to persue Medicine. He is currently working on his Honours project with Dr. Fawcett.",
+            "image": 'avatarMen',
+            width: "65%",
+            width2: "100%",
+            width3: "100%",
+            height: '100%'
+        },
+        {
+            "name": "Kathleen Prior",
+            "role": "Volunteer",
+            "description": "Iain is completing his BSc (Hons) in Behavioural Neuroscience with hopes to persue Medicine. He is currently working on his Honours project with Dr. Fawcett.",
+            "image": 'avatarMen',
+            width: "65%",
+            width2: "100%",
+            width3: "100%",
+            height: '100%'
+        },
+
+        {
+            "name": "Vanessa Strong",
+            "role": "Member",
+            "description": "Liam is a 4th year hons student studying auditory working memory. From his love for music he became interested in how we percieve and understand music, leading to his current research.",
+            "image": 'liam',
+            width: "65%",
+            width2: "100%",
+            width3: "100%",
+        }
+
+    ],
+
+    "MUCEP": [
+        {
+            "name": "Zahin Hasan Prangon",
+            "role": "MUCEP Student",
+            "description": "Zahin is completing his BSc in Computer Science and is currently working on developing the website for the laboratory",
+            "image": 'zahin',
+            width: "65%",
+            width2: "100%",
+            width3: "100%",
         }
     ]
-}
+
+
+
+};
 
 export default teamData;
