ed95504dee15dfa5b03cdfe4438a5252fc632ae1
Zahin430 <zhprangon@mun.ca>
2019-08-27 20:35:03 -0230

    new alumni page and a prop array for alumni

--- src/components/Alumni.js
+++ src/components/Alumni.js
@@ -1,66 +1,46 @@
 import React, { Component } from 'react';
 
-export default class Alumni extends Component {
+import alumdata from './AlumniData';
 
+export default class Alumni extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            collections: alumdata
+        }
+    }
     componentDidMount() {
         window.scrollTo(0, 0);
     }
 
     render() {
-        let alumData = {
-            "members" : [
-                {
-                    "name": "Jonathan Fawcett",
-                    "role": "Lab Director | Assistant Professor",
-                    "description": "Dr. Fawcett is the owner and oporator of the Neuro Fog laboratory. He is currently an Assisant Professor at MUN and has been for the last 3 years.",
-                    "image": 'jon',
-                    width: "40%",
-                    width2: "100%",
-                    width3: "100%",
-                    height: '100%'                  
-                },
-                {
-                    "name": "Jonathan Fawcett",
-                    "role": "Lab Director | Assistant Professor",
-                    "description": "Dr. Fawcett is the owner and oporator of the Neuro Fog laboratory. He is currently an Assisant Professor at MUN and has been for the last 3 years.",
-                    "image": 'jon',
-                    width: "40%",
-                    width2: "100%",
-                    width3: "100%",
-                    height: '100%'  
-                },
-                {
-                    "name": "Jonathan Fawcett",
-                    "role": "Lab Director | Assistant Professor",
-                    "description": "Dr. Fawcett is the owner and oporator of the Neuro Fog laboratory. He is currently an Assisant Professor at MUN and has been for the last 3 years.",
-                    "image": 'jon',
-                    width: "40%",
-                    width2: "100%",
-                    width3: "100%",
-                    height: '100%'  
-                }
-            ]
-        }
-
+        const  { collections} = this.state;
+    
         return (
             <div>
 
                 <section className="team-section">
-                    <h2 className="h1-responsive text-center my-5" style = {{ letterSpacing: '2px' }}>PAST MEMBERS</h2>
+                    <h2 className="h1-responsive text-center my-5" style = {{ letterSpacing: '2px' }}>NEUROFOG LAB'S FORMER MEMBERS</h2>
 
-                    <div className= "row text-center text-md-left" style = {{width: '100%', paddingBottom: '5%'}}>
+                    <div className= "row text-center border text-md-left ml-3 mb-3 text-center justify-content-center z-depth-1 shadow" style = {{width: '95%'}}>
+                    
+                    
+                    <nav class="navbar col-xl-12 z-depth-1 shadow justify-content-center bg-primary text-white ">
+                      <h4 className="h1-responsive font-weight-bold  " style = {{ letterSpacing: '2px' }}>FORMER LAB MANAGERS</h4>
+                    </nav>
                     {
-                        alumData.members && alumData.members.map((item) => {
+                         collections.Managers.map((item) => {
                             return (
-                                    <div id = "team-card" className="col-xl-12 col-lg-12 pb-5 mb-5 ml-5 d-md-flex justify-content-between" >
-                                        <div className="avatar mb-md-0 mb-4 mx-4 team" style = {{ width: item.width || '80%', height: item.height }}>
+                                    <div id = "team-card" className="col-xl-6 col-lg-12 pt-5 mb-5 d-md-flex justify-content-between" >
+                                        <div className="avatar mb-md-0 mb-4 mx-4 team" style = {{ width: item.width || '100%', height: item.height }}>
                                             <img id = "image" src={require(`./img/${item.image}.jpg`)} className="rounded z-depth-1 shadow" style = {{ width: item.width2 || '100%', maxWidth: '100%', height: '100%', maxHeight: '100%' }} alt="avatar"/>
                                         </div>
                             
-                                        <div className="mx-6 justify-content-between" style = {{marginLeft: "50px", width: item.width3 }}>
-                                            <h4 className="font-weight-bold mb-3" style= {{fontSize: "3.5em"}}>{item.name}</h4>
-                                            <h6 className="font-weight-bold grey-text mb-3" style= {{fontSize: "2em"}}>{item.role}</h6>
-                                            <p className="grey-text" style= {{fontSize: "1.5em"}}>{item.description}</p>
+                                        <div className="mx-6" style = {{paddingLeft: item.paddingLeft || '0', width: item.width3 }}>
+                                            <h4 className="font-weight-bold mb-3">{item.name}</h4>
+                                            <h6 className="font-weight-bold grey-text mb-3">{item.role}</h6>
+                                            <p className="grey-text">{item.description}</p>
                                         </div>
                                     </div>
                                 
@@ -68,6 +48,114 @@ export default class Alumni extends Component {
                         })
                     }
                     </div>
+
+
+                    <div className= "row text-center border text-md-left ml-3 mb-3 text-center justify-content-center z-depth-1 shadow" style = {{width: '95%'}}>
+                    
+                    
+                    <nav class="navbar col-xl-12 z-depth-1 shadow justify-content-center bg-primary text-white ">
+                      <h4 className="h1-responsive font-weight-bold  " style = {{ letterSpacing: '2px' }}>FORMER Phd STUDENTS</h4>
+                    </nav>
+                    {
+                         collections.Phd.map((item) => {
+                            return (
+                                    <div id = "team-card" className="col-xl-6 col-lg-12 pt-5 mb-5 d-md-flex justify-content-between" >
+                                        <div className="avatar mb-md-0 mb-4 mx-4 team" style = {{ width: item.width || '100%', height: item.height }}>
+                                            <img id = "image" src={require(`./img/${item.image}.jpg`)} className="rounded z-depth-1 shadow" style = {{ width: item.width2 || '100%', maxWidth: '100%', height: '100%', maxHeight: '100%' }} alt="avatar"/>
+                                        </div>
+                            
+                                        <div className="mx-6" style = {{paddingLeft: item.paddingLeft || '0', width: item.width3 }}>
+                                            <h4 className="font-weight-bold mb-3">{item.name}</h4>
+                                            <h6 className="font-weight-bold grey-text mb-3">{item.role}</h6>
+                                            <p className="grey-text">{item.description}</p>
+                                        </div>
+                                    </div>
+                                
+                            )
+                        })
+                    }
+                    </div>
+
+                    <div className= "row text-center border text-md-left ml-3 mb-3 text-center justify-content-center z-depth-1 shadow" style = {{width: '95%'}}>
+                    
+                    
+                    <nav class="navbar col-xl-12 z-depth-1 shadow justify-content-center bg-primary text-white ">
+                      <h4 className="h1-responsive font-weight-bold  " style = {{ letterSpacing: '2px' }}>FORMER HONORS STUDENTS</h4>
+                    </nav>
+                    {
+                         collections.Honors.map((item) => {
+                            return (
+                                    <div id = "team-card" className="col-xl-6 col-lg-12 pt-5 mb-5 d-md-flex justify-content-between" >
+                                        <div className="avatar mb-md-0 mb-4 mx-4 team" style = {{ width: item.width || '100%', height: item.height }}>
+                                            <img id = "image" src={require(`./img/${item.image}.jpg`)} className="rounded z-depth-1 shadow" style = {{ width: item.width2 || '100%', maxWidth: '100%', height: '100%', maxHeight: '100%' }} alt="avatar"/>
+                                        </div>
+                            
+                                        <div className="mx-6" style = {{paddingLeft: item.paddingLeft || '0', width: item.width3 }}>
+                                            <h4 className="font-weight-bold mb-3">{item.name}</h4>
+                                            <h6 className="font-weight-bold grey-text mb-3">{item.role}</h6>
+                                            <p className="grey-text">{item.description}</p>
+                                        </div>
+                                    </div>
+                                
+                            )
+                        })
+                    }
+                    </div>
+
+
+                    <div className= "row text-center border text-md-left ml-3 mb-3 text-center justify-content-center z-depth-1 shadow" style = {{width: '95%'}}>
+                    
+                    
+                    <nav class="navbar col-xl-12 z-depth-1 shadow justify-content-center bg-primary text-white ">
+                      <h4 className="h1-responsive font-weight-bold  " style = {{ letterSpacing: '2px' }}>FORMER DIRECTED STUDIES</h4>
+                    </nav>
+                    {
+                         collections.directed.map((item) => {
+                            return (
+                                    <div id = "team-card" className="col-xl-6 col-lg-12 pt-5 mb-5 d-md-flex justify-content-between" >
+                                        <div className="avatar mb-md-0 mb-4 mx-4 team" style = {{ width: item.width || '100%', height: item.height }}>
+                                            <img id = "image" src={require(`./img/${item.image}.jpg`)} className="rounded z-depth-1 shadow" style = {{ width: item.width2 || '100%', maxWidth: '100%', height: '100%', maxHeight: '100%' }} alt="avatar"/>
+                                        </div>
+                            
+                                        <div className="mx-6" style = {{paddingLeft: item.paddingLeft || '0', width: item.width3 }}>
+                                            <h4 className="font-weight-bold mb-3">{item.name}</h4>
+                                            <h6 className="font-weight-bold grey-text mb-3">{item.role}</h6>
+                                            <p className="grey-text">{item.description}</p>
+                                        </div>
+                                    </div>
+                                
+                            )
+                        })
+                    }
+                    </div>
+
+
+                    <div className= "row text-center border text-md-left ml-3 mb-3 text-center justify-content-center z-depth-1 shadow" style = {{width: '95%'}}>
+                    
+                    
+                    <nav class="navbar col-xl-12 z-depth-1 shadow justify-content-center bg-primary text-white ">
+                      <h4 className="h1-responsive font-weight-bold  " style = {{ letterSpacing: '2px' }}>FORMER VOLUNTEERS</h4>
+                    </nav>
+                    {
+                         collections.volunteers.map((item) => {
+                            return (
+                                    <div id = "team-card" className="col-xl-6 col-lg-12 pt-5 mb-5 d-md-flex justify-content-between" >
+                                        <div className="avatar mb-md-0 mb-4 mx-4 team" style = {{ width: item.width || '100%', height: item.height }}>
+                                            <img id = "image" src={require(`./img/${item.image}.jpg`)} className="rounded z-depth-1 shadow" style = {{ width: item.width2 || '100%', maxWidth: '100%', height: '100%', maxHeight: '100%' }} alt="avatar"/>
+                                        </div>
+                            
+                                        <div className="mx-6" style = {{paddingLeft: item.paddingLeft || '0', width: item.width3 }}>
+                                            <h4 className="font-weight-bold mb-3">{item.name}</h4>
+                                            <h6 className="font-weight-bold grey-text mb-3">{item.role}</h6>
+                                            <p className="grey-text">{item.description}</p>
+                                        </div>
+                                    </div>
+                                
+                            )
+                        })
+                    }
+                    </div>
+
                 </section>
 
 
---
+++ src/components/AlumniData.js
@@ -0,0 +1,169 @@
+let alumdata = {
+    "Managers": [
+        {
+            "name": "Rachelle Wakeham-Lewis ",
+            "role": "Lab Manager | Honours Student: 2018-2019",
+            "description": "Duis aliquip id elit sunt eu sit consectetur sit ex incididunt excepteur. Incididunt quis aute pariatur consequat veniam consectetur et nisi fugiat. Fugiat excepteur fugiat id cillum in ut. Officia incididunt aliquip anim est minim ipsum. Pariatur nulla pariatur minim esse enim ea irure. Eu sint do eu incididunt exercitation consequat fugiat. Amet reprehenderit adipisicing ut do.",
+            "image": 'rachelle',
+            width: "100%",
+            width2: "100%",
+            width3: "100%",
+            height: '100%'
+        },        
+        {
+            "name": "Julia Greenham ",
+            "role": "Lab Manager: 2017-2018",
+            "description": "Duis aliquip id elit sunt eu sit consectetur sit ex incididunt excepteur. Incididunt quis aute pariatur consequat veniam consectetur et nisi fugiat. Fugiat excepteur fugiat id cillum in ut. Officia incididunt aliquip anim est minim ipsum. Pariatur nulla pariatur minim esse enim ea irure. Eu sint do eu incididunt exercitation consequat fugiat. Amet reprehenderit adipisicing ut do.",
+            "image": 'julia',
+            width: "65%",
+            width2: "100%",
+            width3: "100%",
+            height: '100%'
+        },
+    ],
+
+    "Phd": [
+        {
+            "name": "Landon Churchill",
+            "role": "PhD Student: 2017-2019",
+            "description": "Duis aliquip id elit sunt eu sit consectetur sit ex incididunt excepteur. Incididunt quis aute pariatur consequat veniam consectetur et nisi fugiat. Fugiat excepteur fugiat id cillum in ut. Officia incididunt aliquip anim est minim ipsum. Pariatur nulla pariatur minim esse enim ea irure. Eu sint do eu incididunt exercitation consequat fugiat. Amet reprehenderit adipisicing ut do.",
+            "image": 'landon2',
+            width: "65%",
+            width2: "100%",
+            width3: "100%",
+            height: '100%'
+        },
+        {
+            "name": "Marcus Alves",
+            "role": "Visiting PhD Student: 2016-2017",
+            "description": "Duis aliquip id elit sunt eu sit consectetur sit ex incididunt excepteur. Incididunt quis aute pariatur consequat veniam consectetur et nisi fugiat. Fugiat excepteur fugiat id cillum in ut. Officia incididunt aliquip anim est minim ipsum. Pariatur nulla pariatur minim esse enim ea irure. Eu sint do eu incididunt exercitation consequat fugiat. Amet reprehenderit adipisicing ut do.",
+            "image": 'avatarMen',
+            width: "65%",
+            width2: "100%",
+            width3: "100%",
+            height: '100%'
+        },
+    ],
+    "Honors": [
+        {
+            "name": "Liam Foley",
+            "role": "Honours Student: 2018-2019",
+            "description": "Duis aliquip id elit sunt eu sit consectetur sit ex incididunt excepteur. Incididunt quis aute pariatur consequat veniam consectetur et nisi fugiat. Fugiat excepteur fugiat id cillum in ut. Officia incididunt aliquip anim est minim ipsum. Pariatur nulla pariatur minim esse enim ea irure. Eu sint do eu incididunt exercitation consequat fugiat. Amet reprehenderit adipisicing ut do.",
+            "image": 'liam',
+            width: "85%",
+            width2: "100%",
+            width3: "100%",
+            height: '100%'
+        },
+        {
+            "name": "Iain Gamba",
+            "role": "Honours Student: 2018-2019",
+            "description": "Duis aliquip id elit sunt eu sit consectetur sit ex incididunt excepteur. Incididunt quis aute pariatur consequat veniam consectetur et nisi fugiat. Fugiat excepteur fugiat id cillum in ut. Officia incididunt aliquip anim est minim ipsum. Pariatur nulla pariatur minim esse enim ea irure. Eu sint do eu incididunt exercitation consequat fugiat. Amet reprehenderit adipisicing ut do.",
+            "image": 'iain2',
+            width: "65%",
+            width2: "100%",
+            width3: "100%",
+            height: '100%'
+        },
+        {
+            "name": "Rabia Farmahan",
+            "role": "Honours Student | Directed Studies: 2016-2018",
+            "description": "Duis aliquip id elit sunt eu sit consectetur sit ex incididunt excepteur. Incididunt quis aute pariatur consequat veniam consectetur et nisi fugiat. Fugiat excepteur fugiat id cillum in ut. Officia incididunt aliquip anim est minim ipsum. Pariatur nulla pariatur minim esse enim ea irure. Eu sint do eu incididunt exercitation consequat fugiat. Amet reprehenderit adipisicing ut do.",
+            "image": 'avatarMen',
+            width: "65%",
+            width2: "100%",
+            width3: "100%",
+            height: '100%'
+        },
+        {
+            "name": "Brittany Flood",
+            "role": "Honours Student: 2017-2018",
+            "description": "Duis aliquip id elit sunt eu sit consectetur sit ex incididunt excepteur. Incididunt quis aute pariatur consequat veniam consectetur et nisi fugiat. Fugiat excepteur fugiat id cillum in ut. Officia incididunt aliquip anim est minim ipsum. Pariatur nulla pariatur minim esse enim ea irure. Eu sint do eu incididunt exercitation consequat fugiat. Amet reprehenderit adipisicing ut do.",
+            "image": 'avatarMen',
+            width: "65%",
+            width2: "100%",
+            width3: "100%",
+            height: '100%'
+        },
+        {
+            "name": "Jillian Penney",
+            "role": "Honours Student: 2017-2018",
+            "description": "Duis aliquip id elit sunt eu sit consectetur sit ex incididunt excepteur. Incididunt quis aute pariatur consequat veniam consectetur et nisi fugiat. Fugiat excepteur fugiat id cillum in ut. Officia incididunt aliquip anim est minim ipsum. Pariatur nulla pariatur minim esse enim ea irure. Eu sint do eu incididunt exercitation consequat fugiat. Amet reprehenderit adipisicing ut do.",
+            "image": 'avatarMen',
+            width: "65%",
+            width2: "100%",
+            width3: "100%",
+            height: '100%'
+        },
+    ],
+
+    "directed" : [
+        {
+            "name": "Jenny Tiller",
+            "role": "Directed Studies: 2017-2018",
+            "description": "Duis aliquip id elit sunt eu sit consectetur sit ex incididunt excepteur. Incididunt quis aute pariatur consequat veniam consectetur et nisi fugiat. Fugiat excepteur fugiat id cillum in ut. Officia incididunt aliquip anim est minim ipsum. Pariatur nulla pariatur minim esse enim ea irure. Eu sint do eu incididunt exercitation consequat fugiat. Amet reprehenderit adipisicing ut do.",
+            "image": 'avatarMen',
+            width: "65%",
+            width2: "100%",
+            width3: "100%",
+            height: '100%'
+        },
+        {
+            "name": "Rachel Hewitt",
+            "role": "Directed Studies: 2016-2017",
+            "description": "Duis aliquip id elit sunt eu sit consectetur sit ex incididunt excepteur. Incididunt quis aute pariatur consequat veniam consectetur et nisi fugiat. Fugiat excepteur fugiat id cillum in ut. Officia incididunt aliquip anim est minim ipsum. Pariatur nulla pariatur minim esse enim ea irure. Eu sint do eu incididunt exercitation consequat fugiat. Amet reprehenderit adipisicing ut do.",
+            "image": 'avatarMen',
+            width: "65%",
+            width2: "100%",
+            width3: "100%",
+            height: '100%'
+        },
+    ],
+    "volunteers": [
+        {
+            "name": "Emily Buchanan",
+            "role": "Volunteer: 2016-2018",
+            "description": "Duis aliquip id elit sunt eu sit consectetur sit ex incididunt excepteur. Incididunt quis aute pariatur consequat veniam consectetur et nisi fugiat. Fugiat excepteur fugiat id cillum in ut. Officia incididunt aliquip anim est minim ipsum. Pariatur nulla pariatur minim esse enim ea irure. Eu sint do eu incididunt exercitation consequat fugiat. Amet reprehenderit adipisicing ut do.",
+            "image": 'avatarMen',
+            width: "65%",
+            width2: "100%",
+            width3: "100%",
+            height: '100%'
+        },
+        {
+            "name": "Maryam Michael",
+            "role": "Volunteer: 2016-2017",
+            "description": "Duis aliquip id elit sunt eu sit consectetur sit ex incididunt excepteur. Incididunt quis aute pariatur consequat veniam consectetur et nisi fugiat. Fugiat excepteur fugiat id cillum in ut. Officia incididunt aliquip anim est minim ipsum. Pariatur nulla pariatur minim esse enim ea irure. Eu sint do eu incididunt exercitation consequat fugiat. Amet reprehenderit adipisicing ut do.",
+            "image": 'avatarMen',
+            width: "65%",
+            width2: "100%",
+            width3: "100%",
+            height: '100%'
+        },
+        {
+            "name": "Gabby Hodder",
+            "role": "Volunteer: 2016-2017",
+            "description": "Duis aliquip id elit sunt eu sit consectetur sit ex incididunt excepteur. Incididunt quis aute pariatur consequat veniam consectetur et nisi fugiat. Fugiat excepteur fugiat id cillum in ut. Officia incididunt aliquip anim est minim ipsum. Pariatur nulla pariatur minim esse enim ea irure. Eu sint do eu incididunt exercitation consequat fugiat. Amet reprehenderit adipisicing ut do.",
+            "image": 'avatarMen',
+            width: "65%",
+            width2: "100%",
+            width3: "100%",
+            height: '100%'
+        },
+        {
+            "name": "Lauren Peters",
+            "role": "Volunteer: 2016-2017",
+            "description": "Duis aliquip id elit sunt eu sit consectetur sit ex incididunt excepteur. Incididunt quis aute pariatur consequat veniam consectetur et nisi fugiat. Fugiat excepteur fugiat id cillum in ut. Officia incididunt aliquip anim est minim ipsum. Pariatur nulla pariatur minim esse enim ea irure. Eu sint do eu incididunt exercitation consequat fugiat. Amet reprehenderit adipisicing ut do.",
+            "image": 'avatarMen',
+            width: "65%",
+            width2: "100%",
+            width3: "100%",
+            height: '100%'
+        },
+    ]
+        
+
+    
+}
+
+export default alumdata;
