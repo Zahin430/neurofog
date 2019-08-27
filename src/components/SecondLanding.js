4bec984805e64c0015c2221296d4b9f2f7dee158
Zahin430 <zhprangon@mun.ca>
2019-08-27 20:37:13 -0230

    added prop array to map over card component

--- src/components/SecondLanding.js
+++ src/components/SecondLanding.js
@@ -1,40 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
-import researchMain from './img/researchMain.jpg'
-import participation from './img/participation_2.jpeg'
+// import participation from './img/participation_2.jpeg'
 
 
-const SecondLanding = () => {
+export default class SecondLanding extends Component {
+  render() {
+    let items = {
+      "card": [
+        {
+          "image" : "teamsmall.jpg",
+          "link" : "Our Team",
+          "route": "/team",
+          "desc": "View our current and past members of the Neuro Fog Laboratory.",
+          "height": "67%"
+
+        },
+        {
+          "image" : "researchMain.jpg",
+          "link" : "Our Research",
+          "route": "/research",
+          "desc": "Take a look at what research goes on in our laboratory.",
+          "height": "67%"
+        },
+        {
+          "image" : "participation_2.jpeg",
+          "link" : "Become a Participant",
+          "route": "/involved",
+          "desc": "Interested in participting in studies? Click to find out more!",
+          "height": "67%"
+        }
+
+      ]
+    }
     return (
-    <div className="card-deck" style= {{padding: '5%' , width: '100%', height: '100%'}}>
-    <div className="card shadow bg-white rounded" id ="card1">
-    <img className="card-img-top" src="http://neurofog.ca/img/teamsmall.jpg" alt="Card1"/>
-    <div className="card-body">
-      <h5 className="card-title">
-      <Link className="btn btn-primary" style={{ textDecoration: 'none', color: 'white', objectFit: 'contain',  height: 'auto' }} to ="/team">Our Team</Link>
-      </h5>
-      <p className="card-text">View our current and past members of the Neuro Fog Laboratory.</p>
-    </div>
-  </div>
-  <div className="card shadow bg-white rounded" id ="card2">
-    <img className="card-img-top" style= {{ height: '67%', width: '100%' }} src={researchMain} alt="Card2"/>
-    <div className="card-body">
-      <h5 className="card-title">
-      <Link className="btn btn-primary" style={{ textDecoration: 'none', color: 'white' }} to ="/research">Our Research</Link>
-      </h5>
-      <p className="card-text">Take a look at what research goes on in our laboratory.</p>
-    </div>
-  </div>
-  <div className="card shadow bg-white rounded" id ="card3">
-    <img className="card-img-top" src={participation}  style= {{ height: '67%'}} alt="Card3"/>
-    <div className="card-body">
-      <h5>
-        <Link className="btn btn-primary" style={{ textDecoration: 'none', color: 'white' }} to ="/involved">Become a Participant</Link>
-      </h5>
-      <p className="card-text">Interested in participting in studies? Click to find out more!</p>
-    </div>
-  </div>
-</div>
-    )
+      <div className="card-deck" style={{ padding: '5%', width: '100%', height: '100%' }}>
+      {
+        items.card.map((item) => {
+          return (
+            <div className="card shadow bg-white rounded" id="card1">
+            <img className="card-img-top" src={require(`./img/${item.image}`)} style = {{height: item.height}} alt="Card1" />
+            <div className="card-body">
+              <h5 className="card-title">
+                <Link className="btn btn-primary" style={{ textDecoration: 'none', color: 'white' }} to= {item.route}>{item.link}</Link>
+              </h5>
+              <p className="card-text">{item.desc}</p>
+            </div>
+          </div>
+          )
+        })
+      }
+      
+
+        </div>
+        )
+  }
+
 }
-export default SecondLanding;
+
