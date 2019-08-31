4cd95b4541a0886cfae3e8dcaacd073627660b2a
Zahin430 <zhprangon@mun.ca>
2019-08-31 00:27:25 -0230

    moved all the publications to a component

--- src/components/Publication.js
+++ src/components/Publication.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 import document1 from './Website PDFs/fawcett2016-1.pdf'
 import document2 from './Website PDFs/fawcett2016 - 2.pdf'
@@ -12,87 +12,133 @@ import document7 from './Website PDFs/bodner2013 - 7.pdf'
 import document8 from './Website PDFs/fawcett2013 - 8.pdf'
 import document9 from './Website PDFs/russell2013 - 9.pdf'
 import document10 from './Website PDFs/fawcett2013 - 10.pdf'
+import document11 from './Website PDFs/document11.pdf'
+import document12 from './Website PDFs/document12.pdf'
+import document13 from './Website PDFs/doc13.pdf'
+import document14 from './Website PDFs/doc14.pdf'
+
 
 const Publication = () => {
     return (
 
         <div>
-        <h3 id = "list-research-header" className = "mx-auto" style = {{textAlign: 'center' }}>List of publications</h3>
-        
-        <div className = "mx-auto row" id = "list-publication">
+            <h3 id="list-research-header" className="mx-auto" style={{ textAlign: 'center' }}>List of publications</h3>
 
-          
-          <div className="list-group col-sm w-100 p-3">
-            <a  href="https://www.sciencedirect.com/science/article/pii/S2211368116300699" target="_blank" rel="noopener noreferrer" className="list-group-item list-group-item-action ">Looking Down the Barrel of a Gun: What Do We Know About the Weapon Focus Effect? <span className="text-muted">(2014)
-            <Link
-                to={document1}
-                target="_blank" download style={{paddingLeft: '4px'}}>
-                (pdf)  
-            </Link></span></a>
+            <div className="mx-auto row" id="list-publication">
 
 
-            <a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4886847/" target="_blank" rel="noopener noreferrer" className="list-group-item list-group-item-action">Familiarity, but not recollection, supports the between-subject production effect. <span className="text-muted">(2016)
+
+
+            <div className="list-group col-sm w-100 p-3">
+                    
+
+            <a href="https://www.psychiatrist.com/JCP/article/Pages/2019/v80/18r12527.aspx" target="_blank" rel="noopener noreferrer" className="list-group-item list-group-item-action ">The Prevalence of Anxiety Disorders During Pregnancy and the Postpartum Period: A Multivariate Bayesian Meta-Analysis.<span className="text-muted"> (2019)
+            </span></a>
+
+
+            <a href="https://www.sciencedirect.com/science/article/pii/S0010027719300551" target="_blank" rel="noopener noreferrer" className="list-group-item list-group-item-action ">Reconsidering unconscious persistence: Suppressing unwanted memories reduces their indirect expression in later thoughts.<span className="text-muted"> (2019)
             <Link
-                to={document2}
-                target="_blank" download style={{paddingLeft: '4px'}}>
-                (pdf)  
+                            to={document11}
+                            target="_blank" download style={{ paddingLeft: '4px' }}>
+                            (pdf)
             </Link></span></a>
 
-            <a href="https://www.ncbi.nlm.nih.gov/pubmed/26709589" target="_blank" rel="noopener noreferrer" className="list-group-item list-group-item-action">The representational consequences of intentional forgetting: Impairments to both the probability and fidelity of long-term memory. <span className="text-muted">(2016)
+            <a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC6383646/" target="_blank" rel="noopener noreferrer" className="list-group-item list-group-item-action ">A Bayesian multivariate approach to estimating the prevalence of a superordinate category of disorders<span className="text-muted"> (2018)
             <Link
-                to={document3}
-                target="_blank" download style={{paddingLeft: '4px'}}>
-                (pdf)  
+                            to={document12}
+                            target="_blank" download style={{ paddingLeft: '4px' }}>
+                            (pdf)
             </Link></span></a>
-            <a href="https://www.ncbi.nlm.nih.gov/pubmed/24853535" target="_blank" rel="noopener noreferrer" className="list-group-item list-group-item-action">Effects of distinctive encoding on correct and false memory: A meta-analytic review of costs and benefits and their origins in the DRM paradigm. <span className="text-muted">(2015)
-            <Link
-                to={document4}
-                target="_blank" download style={{paddingLeft: '4px'}}>
-                (pdf)  
-            </Link></span></a>
-            <a href="https://www.ncbi.nlm.nih.gov/pubmed/25462596" target="_blank" rel="noopener noreferrer" className="list-group-item list-group-item-action ">The origins of repetitive thought in rumination: Separating cognitive style from deficits in inhibitory control over memory. <span className="text-muted">(2015)
-            <Link
-                to={document5}
-                target="_blank" download style={{paddingLeft: '4px'}}>
-                (pdf)  
-            </Link></span></a>  
-          </div>
 
-          <div className="list-group col-sm w-100 p-3">
-            <a href="https://www.ncbi.nlm.nih.gov/pubmed/24064067" target="_blank" rel="noopener noreferrer" className="list-group-item list-group-item-action">Electrophysiological markers of biological motion and human form recognition. <span className="text-muted">(2014)
+            <a href="https://www.ncbi.nlm.nih.gov/pubmed/29672641" target="_blank" rel="noopener noreferrer" className="list-group-item list-group-item-action ">Inducing preference reversals in aesthetic choices for paintings: Introducing the contrast paradigm.<span className="text-muted"> (2018)
             <Link
-                to={document6}
-                target="_blank" download style={{paddingLeft: '4px'}}>
-                (pdf)  
+                            to={document13}
+                            target="_blank" download style={{ paddingLeft: '4px' }}>
+                            (pdf)
             </Link></span></a>
-            <a href="https://www.ncbi.nlm.nih.gov/pubmed/23884689" target="_blank" rel="noopener noreferrer" className="list-group-item list-group-item-action">The costs and benefits of production in recognition. <span className="text-muted">(2014)
-            <Link
-                to={document7}
-                target="_blank" download style={{paddingLeft: '4px'}}>
-                (pdf)  
-            </Link></span></a>
-            <a href="https://www.sciencedirect.com/science/article/pii/S0001691813001571" target="_blank" rel="noopener noreferrer" style = {{ paddingBottom: '20px'}} className="list-group-item list-group-item-action">Event-method directed forgetting: Forgetting a video segment is more effortful than remembering it. <span className="text-muted">(2013)
-            <Link
-                to={document8}
-                target="_blank" download style={{paddingLeft: '4px'}}>
-                (pdf)  
-            </Link></span></a>
-            <a href="https://www.ncbi.nlm.nih.gov/pubmed/23656845" target="_blank" rel="noopener noreferrer" style = {{ paddingBottom: '20px'}} className="list-group-item list-group-item-action">Risk of obsessive-compulsive disorder in pregnant and post-partum women: A meta-analysis <span className="text-muted">(2013)
-            <Link
-                to={document9}
-                target="_blank" download style={{paddingLeft: '4px'}}>
-                (pdf)  
-            </Link></span></a>
-            <a href="https://www.sciencedirect.com/science/article/pii/S0001691812001680" target="_blank" rel="noopener noreferrer" style = {{ paddingBottom: '20px'}} className="list-group-item list-group-item-action ">The production effect benefits performance in between-subject designs: A meta-analysis. <span className="text-muted">(2013)
-            <Link
-                to={document10}
-                target="_blank" download style={{paddingLeft: '4px'}}>
-                (pdf)  
-            </Link></span></a>
-          </div>
 
-      </div>
-      </div>
+            <a href="https://www.researchgate.net/publication/291523513_A_meta-analysis_of_the_worldwide_prevalence_of_pica_during_pregnancy_and_the_postpartum_period" target="_blank" rel="noopener noreferrer" className="list-group-item list-group-item-action ">A meta-analysis of the worldwide prevalence of pica during pregnancy and the postpartum period.<span className="text-muted"> (2016)
+            <Link
+                            to={document14}
+                            target="_blank" download style={{ paddingLeft: '4px' }}>
+                            (pdf)
+            </Link></span></a>
+
+
+
+                </div>
+
+                <div className="list-group col-sm w-100 p-3">
+                    <a href="https://www.sciencedirect.com/science/article/pii/S2211368116300699" target="_blank" rel="noopener noreferrer" className="list-group-item list-group-item-action ">Looking Down the Barrel of a Gun: What Do We Know About the Weapon Focus Effect? <span className="text-muted">(2014)
+            <Link
+                            to={document1}
+                            target="_blank" download style={{ paddingLeft: '4px' }}>
+                            (pdf)
+            </Link></span></a>
+
+
+                    <a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4886847/" target="_blank" rel="noopener noreferrer" className="list-group-item list-group-item-action">Familiarity, but not recollection, supports the between-subject production effect. <span className="text-muted">(2016)
+            <Link
+                            to={document2}
+                            target="_blank" download style={{ paddingLeft: '4px' }}>
+                            (pdf)
+            </Link></span></a>
+
+                    <a href="https://www.ncbi.nlm.nih.gov/pubmed/26709589" target="_blank" rel="noopener noreferrer" className="list-group-item list-group-item-action">The representational consequences of intentional forgetting: Impairments to both the probability and fidelity of long-term memory. <span className="text-muted">(2016)
+            <Link
+                            to={document3}
+                            target="_blank" download style={{ paddingLeft: '4px' }}>
+                            (pdf)
+            </Link></span></a>
+                    <a href="https://www.ncbi.nlm.nih.gov/pubmed/24853535" target="_blank" rel="noopener noreferrer" className="list-group-item list-group-item-action">Effects of distinctive encoding on correct and false memory: A meta-analytic review of costs and benefits and their origins in the DRM paradigm. <span className="text-muted">(2015)
+            <Link
+                            to={document4}
+                            target="_blank" download style={{ paddingLeft: '4px' }}>
+                            (pdf)
+            </Link></span></a>
+                    <a href="https://www.ncbi.nlm.nih.gov/pubmed/25462596" target="_blank" rel="noopener noreferrer" className="list-group-item list-group-item-action ">The origins of repetitive thought in rumination: Separating cognitive style from deficits in inhibitory control over memory. <span className="text-muted">(2015)
+            <Link
+                            to={document5}
+                            target="_blank" download style={{ paddingLeft: '4px' }}>
+                            (pdf)
+            </Link></span></a>
+                </div>
+
+                <div className="list-group col-sm w-100 p-3">
+                    <a href="https://www.ncbi.nlm.nih.gov/pubmed/24064067" target="_blank" rel="noopener noreferrer" className="list-group-item list-group-item-action">Electrophysiological markers of biological motion and human form recognition. <span className="text-muted">(2014)
+            <Link
+                            to={document6}
+                            target="_blank" download style={{ paddingLeft: '4px' }}>
+                            (pdf)
+            </Link></span></a>
+                    <a href="https://www.ncbi.nlm.nih.gov/pubmed/23884689" target="_blank" rel="noopener noreferrer" className="list-group-item list-group-item-action">The costs and benefits of production in recognition. <span className="text-muted">(2014)
+            <Link
+                            to={document7}
+                            target="_blank" download style={{ paddingLeft: '4px' }}>
+                            (pdf)
+            </Link></span></a>
+                    <a href="https://www.sciencedirect.com/science/article/pii/S0001691813001571" target="_blank" rel="noopener noreferrer" style={{ paddingBottom: '20px' }} className="list-group-item list-group-item-action">Event-method directed forgetting: Forgetting a video segment is more effortful than remembering it. <span className="text-muted">(2013)
+            <Link
+                            to={document8}
+                            target="_blank" download style={{ paddingLeft: '4px' }}>
+                            (pdf)
+            </Link></span></a>
+                    <a href="https://www.ncbi.nlm.nih.gov/pubmed/23656845" target="_blank" rel="noopener noreferrer" style={{ paddingBottom: '20px' }} className="list-group-item list-group-item-action">Risk of obsessive-compulsive disorder in pregnant and post-partum women: A meta-analysis <span className="text-muted">(2013)
+            <Link
+                            to={document9}
+                            target="_blank" download style={{ paddingLeft: '4px' }}>
+                            (pdf)
+            </Link></span></a>
+                    <a href="https://www.sciencedirect.com/science/article/pii/S0001691812001680" target="_blank" rel="noopener noreferrer" style={{ paddingBottom: '20px' }} className="list-group-item list-group-item-action ">The production effect benefits performance in between-subject designs: A meta-analysis. <span className="text-muted">(2013)
+            <Link
+                            to={document10}
+                            target="_blank" download style={{ paddingLeft: '4px' }}>
+                            (pdf)
+            </Link></span></a>
+                </div>
+
+            </div>
+        </div>
     );
 
 }
