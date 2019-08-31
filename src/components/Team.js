048633f962ffbd42a858834236dbb7587fcc987a
Zahin430 <zhprangon@mun.ca>
2019-08-31 00:28:18 -0230

    team page layour changed and made responsive

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 
-import jon from './img/jon.jpg'
 import teamData from './teamData'
 
 export default class Team extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
 
         this.state = {
@@ -17,138 +16,148 @@ export default class Team extends Component {
     }
 
     render() {
-        const {collections} = this.state;
-    
-    return (
-        <div> 
-        <section className="team-section">
-                    <h2 className="h1-responsive text-center my-5" style = {{ letterSpacing: '2px' }}>NEURO FOG LABORATORY</h2>
+        const { collections } = this.state;
+
+        return (
+            <div className='container-fluid'>
+                <section className="team-section">
+                    <h2 className="h1-responsive text-center justify-content-center my-5" style={{ letterSpacing: '2px' }}>NEURO FOG LABORATORY</h2>
+
+                    <div className="row text-center border text-md-left ml-3 mb-3 text-center justify-content-start z-depth-1 shadow" style={{ width: '95%' }}>
 
 
-                    <div className= "row text-md-left text-center justify-content-center " style = {{width: '95%', paddingBottom: '5%', marginLeft: '50px'}}>
-              
-                    <div id = "team-card card" className="col-xl-6 col-lg-12 mb-5 d-md-flex justify-content-between" >
-                            <div className="avatar mb-md-0 mb-4 mx-4 team" style = {{ width: '65%', height: '100%' }}>
-                                <img id = "image" src={jon} className="rounded z-depth-1 shadow" style = {{ width: '100%', maxWidth: '100%', height: '100%', maxHeight: '100%' }} alt="avatar"/>
-                            </div>
-                
-                            <div className="mx-12" style = {{paddingLeft: '0', width: '100%' }}>
-                            <h4 className="font-weight-bold mb-3">Jonathan Fawcett</h4>
-                            <h6 className="font-weight-bold grey-text mb-3">Lab Director | Assistant Professor</h6>
-                                <p className="grey-text">Dr. Fawcett is the owner and operator of the Neuro Fog laboratory. He is currently an Assisant Professor at MUN and has been for the last 3 years.</p>
-                    </div>
-                    </div>
-                    </div>
-                    <div className= "row text-center border text-md-left ml-3 mb-3 text-center justify-content-center z-depth-1 shadow" style = {{width: '95%'}}>
-                    
-                    
-                    <nav class="navbar col-xl-12 z-depth-1 shadow justify-content-center bg-primary text-white ">
-                      <h4 className="h1-responsive font-weight-bold  " style = {{ letterSpacing: '2px' }}>MASTERS STUDENTS</h4>
-                    </nav>
-                    {
-                         collections.MSC.map((item) => {
-                            return (
-                                    <div id = "team-card" className="col-xl-6 col-lg-12 pt-5 mb-5 d-md-flex justify-content-between" >
-                                        <div className="avatar mb-md-0 mb-4 mx-4 team" style = {{ width: item.width || '100%', height: item.height }}>
-                                            <img id = "image" src={require(`./img/${item.image}.jpg`)} className="rounded z-depth-1 shadow" style = {{ width: item.width2 || '100%', maxWidth: '100%', height: '100%', maxHeight: '100%' }} alt="avatar"/>
+                        <nav class="navbar col-xl-12 z-depth-1 shadow justify-content-center text-white" style={{ backgroundColor: "#47AFAF" }}>
+                            <h4 className="h1-responsive font-weight-bold  " style={{ letterSpacing: '2px' }}>LAB DIRECTOR</h4>
+                        </nav>
+                        {
+                            collections.director.map((item) => {
+                                return (
+                                    <div id="team-card" className="col-xl-6 col-md-12 col-sm-12 col-lg-12 pt-5 mb-5 d-md-flex justify-content-center" >
+                                        <div className="img-responsive mb-4 mr-4 team" style={{ width: item.width || '100%', height: item.height }}>
+                                            <img id="image" src={require(`./img/${item.image}.jpg`)} className="rounded img-responsive z-depth-1 shadow" style={{ width: item.width2 || '100%', height: '100%' }} alt="avatar" />
                                         </div>
-                            
-                                        <div className="mx-6" style = {{paddingLeft: item.paddingLeft || '0', width: item.width3 }}>
+
+                                        <div className="col-md-6" style={{ width: item.width3 }}>
                                             <h4 className="font-weight-bold mb-3">{item.name}</h4>
                                             <h6 className="font-weight-bold grey-text mb-3">{item.role}</h6>
                                             <p className="grey-text">{item.description}</p>
                                         </div>
                                     </div>
-                                
-                            )
-                        })
-                    }
+
+                                )
+                            })
+                        }
                     </div>
-                    
-                    <div className= "row text-center border text-md-left ml-3 pb-2 text-center justify-content-center z-depth-1 shadow rounded" style = {{width: '95%'}}>
-
-                    <nav class="navbar col-xl-12 z-depth-1 shadow justify-content-center bg-primary text-white ">
-                      <h4 className="h1-responsive font-weight-bold  " style = {{ letterSpacing: '2px' }}>HONORS STUDENTS</h4>
-                    </nav>
-                    {
-                      collections.Honors.map((item) => {
-                         return (
-                                 <div id = "team-card" className="col-xl-6 col-lg-12 mb-5 pt-5 d-md-flex justify-content-between" >
-                                     <div className="avatar mb-md-0 mb-4 mx-4 team" style = {{ width: item.width || '100%', height: item.height }}>
-                                         <img id = "image" src={require(`./img/${item.image}.jpg`)} className="rounded z-depth-1 shadow" style = {{ width: item.width2 || '100%', maxWidth: '100%', height: '100%', maxHeight: '100%' }} alt="avatar"/>
-                                     </div>
-                         
-                                     <div className="mx-6" style = {{paddingLeft: item.paddingLeft || '0', width: item.width3 }}>
-                                         <h4 className="font-weight-bold mb-3">{item.name}</h4>
-                                         <h6 className="font-weight-bold grey-text mb-3">{item.role}</h6>
-                                         <p className="grey-text">{item.description}</p>
-                                     </div>
-                                 </div>
-                             
-                         )
-                     })
-                 }
-                 </div>
 
 
-                 <div className= "row text-center border text-md-left ml-3 mb-3 mt-3 text-center justify-content-center z-depth-1 shadow" style = {{width: '95%'}}>
-                    
-                    
-                 <nav class="navbar col-xl-12 z-depth-1 shadow justify-content-center bg-primary text-white ">
-                   <h4 className="h1-responsive font-weight-bold  " style = {{ letterSpacing: '2px' }}>Volunteers</h4>
-                 </nav>
-                 {
-                      collections.Volunteers.map((item) => {
-                         return (
-                                 <div id = "team-card" className="col-xl-6 col-lg-12 pt-5 mb-5 d-md-flex justify-content-between" >
-                                     <div className="avatar mb-md-0 mb-4 mx-4 team" style = {{ width: item.width || '100%', height: item.height }}>
-                                         <img id = "image" src={require(`./img/${item.image}.jpg`)} className="rounded z-depth-1 shadow" style = {{ width: item.width2 || '100%', maxWidth: '100%', height: '100%', maxHeight: '100%' }} alt="avatar"/>
-                                     </div>
-                         
-                                     <div className="mx-6" style = {{paddingLeft: item.paddingLeft || '0', width: item.width3 }}>
-                                         <h4 className="font-weight-bold mb-3">{item.name}</h4>
-                                         <h6 className="font-weight-bold grey-text mb-3">{item.role}</h6>
-                                         <p className="grey-text">{item.description}</p>
-                                     </div>
-                                 </div>
-                             
-                         )
-                     })
-                 }
-                 </div>
+
+                    <div className="row text-center border text-md-left ml-3 mb-3 text-center justify-content-start z-depth-1 shadow" style={{ width: '95%' }}>
 
 
-                 <div className= "row text-center border text-md-left ml-3 mb-3 mt-2 text-center justify-content-center z-depth-1 shadow" style = {{width: '95%'}}>
-                    
-                    
-                 <nav class="navbar col-xl-12 z-depth-1 shadow justify-content-center bg-primary text-white ">
-                   <h4 className="h1-responsive font-weight-bold  " style = {{ letterSpacing: '2px' }}>MUCEP STUDENTS</h4>
-                 </nav>
-                 {
-                      collections.MUCEP.map((item) => {
-                         return (
-                                 <div id = "team-card" className="col-xl-6 col-lg-12 pt-5 mb-5 d-md-flex justify-content-between" >
-                                     <div className="avatar mb-md-0 mb-4 mx-4 team" style = {{ width: item.width || '100%', height: item.height }}>
-                                         <img id = "image" src={require(`./img/${item.image}.jpg`)} className="rounded z-depth-1 shadow" style = {{ width: item.width2 || '100%', maxWidth: '100%', height: '100%', maxHeight: '100%' }} alt="avatar"/>
-                                     </div>
-                         
-                                     <div className="mx-6" style = {{paddingLeft: item.paddingLeft || '0', width: item.width3 }}>
-                                         <h4 className="font-weight-bold mb-3">{item.name}</h4>
-                                         <h6 className="font-weight-bold grey-text mb-3">{item.role}</h6>
-                                         <p className="grey-text">{item.description}</p>
-                                     </div>
-                                 </div>
-                             
-                         )
-                     })
-                 }
-                 </div>
-                 
-                 
-                 </section>
-                 
-        </div>
-        
-    );
+                        <nav class="navbar col-xl-12 z-depth-1 shadow justify-content-center text-white" style={{ backgroundColor: "#47AFAF" }}>
+                            <h4 className="h1-responsive font-weight-bold  " style={{ letterSpacing: '2px' }}>MASTERS STUDENTS</h4>
+                        </nav>
+                        {
+                            collections.MSC.map((item) => {
+                                return (
+                                    <div id="team-card" className="col-xl-6 col-md-12 col-sm-12 col-lg-12 pt-5 mb-5 d-md-flex justify-content-center" >
+                                        <div className="img-responsive mb-4 mr-4 team" style={{ width: item.width || '100%', height: item.height }}>
+                                            <img id="image" src={require(`./img/${item.image}.jpg`)} className="rounded img-responsive z-depth-1 shadow" style={{ width: item.width2 || '100%', height: '100%' }} alt="avatar" />
+                                        </div>
+
+                                        <div className="col-md-6" style={{ width: item.width3 }}>
+                                            <h4 className="font-weight-bold mb-3">{item.name}</h4>
+                                            <h6 className="font-weight-bold grey-text mb-3">{item.role}</h6>
+                                            <p className="grey-text">{item.description}</p>
+                                        </div>
+                                    </div>
+
+                                )
+                            })
+                        }
+                    </div>
+
+                    <div className="row text-center border text-md-left ml-3 pb-2 text-center justify-content-center z-depth-1 shadow rounded" style={{ width: '95%' }}>
+
+                        <nav class="navbar col-xl-12 z-depth-1 shadow justify-content-center text-white" style={{ backgroundColor: "#47AFAF" }}>
+                            <h4 className="h1-responsive font-weight-bold " style={{ letterSpacing: '2px' }}>HONORS STUDENTS</h4>
+                        </nav>
+                        {
+                            collections.Honors.map((item) => {
+                                return (
+                                    <div id="team-card" className="col-xl-6 col-md-12 col-sm-12 col-lg-12 pt-5 mb-5 d-md-flex justify-content-center" >
+                                        <div className="img-responsive mb-4 mr-4 team" style={{ width: item.width || '100%', height: item.height }}>
+                                            <img id="image" src={require(`./img/${item.image}.jpg`)} className="rounded img-responsive z-depth-1 shadow" style={{ width: item.width2 || '100%', height: '100%' }} alt="avatar" />
+                                        </div>
+
+                                        <div className="col-md-6" style={{ width: item.width3 }}>
+                                            <h4 className="font-weight-bold mb-3">{item.name}</h4>
+                                            <h6 className="font-weight-bold grey-text mb-3">{item.role}</h6>
+                                            <p className="grey-text">{item.description}</p>
+                                        </div>
+                                    </div>
+
+                                )
+                            })
+                        }
+                    </div>
+
+
+                    <div className="row text-center border text-md-left ml-3 mb-3 mt-3 text-center justify-content-center z-depth-1 shadow" style={{ width: '95%' }}>
+
+                        <nav class="navbar col-xl-12 z-depth-1 shadow justify-content-center text-white" style={{ backgroundColor: "#47AFAF" }}>
+                            <h4 className="h1-responsive font-weight-bold  " style={{ letterSpacing: '2px' }}>VOLUNTEERS</h4>
+                        </nav>
+                        {
+                            collections.Volunteers.map((item) => {
+                                return (
+                                    <div id="team-card" className="col-xl-6 col-md-12 col-sm-12 col-lg-12 pt-5 mb-5 d-md-flex justify-content-center" >
+                                        <div className="img-responsive mb-4 mr-4 team" style={{ width: item.width || '100%', height: item.height }}>
+                                            <img id="image" src={require(`./img/${item.image}.jpg`)} className="rounded img-responsive z-depth-1 shadow" style={{ width: item.width2 || '100%', height: '100%' }} alt="avatar" />
+                                        </div>
+
+                                        <div className="col-md-6" style={{ width: item.width3 }}>
+                                            <h4 className="font-weight-bold mb-3">{item.name}</h4>
+                                            <h6 className="font-weight-bold grey-text mb-3">{item.role}</h6>
+                                            <p className="grey-text">{item.description}</p>
+                                        </div>
+                                    </div>
+
+                                )
+                            })
+                        }
+                    </div>
+
+
+                    <div className="row text-center border text-md-left ml-3 mb-3 mt-2 text-center justify-content-start z-depth-1 shadow" style={{ width: '95%' }}>
+
+                        <nav class="navbar col-xl-12 z-depth-1 shadow justify-content-center text-white" style={{ backgroundColor: "#47AFAF" }}>
+                            <h4 className="h1-responsive font-weight-bold  " style={{ letterSpacing: '2px' }}>MUCEP STUDENTS</h4>
+                        </nav>
+                        {
+                            collections.MUCEP.map((item) => {
+                                return (
+                                    <div id="team-card" className="col-xl-6 col-md-12 col-sm-12 col-lg-12 pt-5 mb-5 d-md-flex justify-content-center" >
+                                        <div className="img-responsive mb-4 mr-4 team" style={{ width: item.width || '100%', height: item.height }}>
+                                            <img id="image" src={require(`./img/${item.image}.jpg`)} className="rounded img-responsive z-depth-1 shadow" style={{ width: item.width2 || '100%', height: '100%' }} alt="avatar" />
+                                        </div>
+
+                                        <div className="col-md-6" style={{ width: item.width3 }}>
+                                            <h4 className="font-weight-bold mb-3">{item.name}</h4>
+                                            <h6 className="font-weight-bold grey-text mb-3">{item.role}</h6>
+                                            <p className="grey-text">{item.description}</p>
+                                        </div>
+                                    </div>
+
+                                )
+                            })
+                        }
+                    </div>
+
+                </section>
+
+            </div>
+
+        );
     }
 }
--- src/components/teamData.js
+++ src/components/teamData.js
@@ -1,11 +1,23 @@
 let teamData = {
+    "director" : [
+        {
+            "name": "Jonathan Fawcett",
+            "role": "Lab Director | Assistant Professor",
+            "description": "Dr. Fawcett is the owner and operator of the Neuro Fog laboratory. He is currently an Assisant Professor at MUN and has been for the last 3 years.",
+            "image": 'jon',
+            width: "100%",
+            width2: "100%",
+            width3: "100%",
+            height: '100%'
+        }
+    ],
     "MSC": [
         {
             "name": "Chris Clarke",
             "role": "MSc Student",
             "description": "Duis aliquip id elit sunt eu sit consectetur sit ex incididunt excepteur. Incididunt quis aute pariatur consequat veniam consectetur et nisi fugiat. Fugiat excepteur fugiat id cillum in ut. Officia incididunt aliquip anim est minim ipsum. Pariatur nulla pariatur minim esse enim ea irure. Eu sint do eu incididunt exercitation consequat fugiat. Amet reprehenderit adipisicing ut do.",
             "image": 'avatarMen',
-            width: "65%",
+            width: "100%",
             width2: "100%",
             width3: "100%",
             height: '100%'
@@ -16,7 +28,7 @@ let teamData = {
             "role": "MSc Student (Co-supervised w/ Dr. Emily Fawcett)",
             "description": "Duis aliquip id elit sunt eu sit consectetur sit ex incididunt excepteur. Incididunt quis aute pariatur consequat veniam consectetur et nisi fugiat. Fugiat excepteur fugiat id cillum in ut. Officia incididunt aliquip anim est minim ipsum. Pariatur nulla pariatur minim esse enim ea irure. Eu sint do eu incididunt exercitation consequat fugiat. Amet reprehenderit adipisicing ut do.",
             "image": 'avatarMen',
-            width: "65%",
+            width: "100%",
             width2: "100%",
             width3: "100%",
             height: '100%'
@@ -27,7 +39,7 @@ let teamData = {
             "role": "MSc Student",
             "description": "Duis aliquip id elit sunt eu sit consectetur sit ex incididunt excepteur. Incididunt quis aute pariatur consequat veniam consectetur et nisi fugiat. Fugiat excepteur fugiat id cillum in ut. Officia incididunt aliquip anim est minim ipsum. Pariatur nulla pariatur minim esse enim ea irure. Eu sint do eu incididunt exercitation consequat fugiat. Amet reprehenderit adipisicing ut do.",
             "image": 'kelsi',
-            width: "65%",
+            width: "100%",
             width2: "100%",
             width3: "100%",
             height: '100%'
@@ -38,7 +50,7 @@ let teamData = {
             "role": "MSc Student (Co-supervised w/ Dr. Joshua Rash)",
             "description": "Duis aliquip id elit sunt eu sit consectetur sit ex incididunt excepteur. Incididunt quis aute pariatur consequat veniam consectetur et nisi fugiat. Fugiat excepteur fugiat id cillum in ut. Officia incididunt aliquip anim est minim ipsum. Pariatur nulla pariatur minim esse enim ea irure. Eu sint do eu incididunt exercitation consequat fugiat. Amet reprehenderit adipisicing ut do.",
             "image": 'avatarMen',
-            width: "65%",
+            width: "100%",
             width2: "100%",
             width3: "100%",
             height: '100%'
@@ -46,7 +58,7 @@ let teamData = {
         },
         {
             "name": "Hannah Willoughby ",
-            "role": "Masters",
+            "role": "MSc Student",
             "description": "Hannah is a Master's student and has been involved in various studies examining recognition and recall memory. Currently, she is interested in using EEG to further her understanding of the cognitive processes underlying our memory. ",
             "image": 'hannah',
             width: "100%",
@@ -59,27 +71,6 @@ let teamData = {
     ],
     "Honors": [
         {
-            "name": "Michael Foote ",
-            "role": "Honors Student (Co-supervised w/ Dr. Michelle Ploughman)",
-            "description": "Julia is currently completing her BSc (Hons) in Psychology. She is working with Dr. Fawcett and Dr. Rash to determine the cognitive mechanisms involved in managing past and future painful events.",
-            "image": 'juliarose21',
-            width: "65%",
-            width2: "100%",
-            width3: "100%",
-            height: '100%'
-
-        },
-        {
-            "name": "Janaya Toope ",
-            "role": "Honours Student ",
-            "description": "Member of the Laboratory.",
-            "image": 'avatarMen',
-            width: "65%",
-            width2: "100%",
-            width3: "100%",
-            height: '100%'
-        },
-        {
             "name": "Chelsea Lahey",
             "role": "Honours Student ",
             "description": "Member of the Laboratory.",
@@ -90,10 +81,31 @@ let teamData = {
             height: '100%'
         },
         {
+            "name": "Michael Foote ",
+            "role": "Honors Student (Co-supervised w/ Dr. Michelle Ploughman)",
+            "description": "Julia is currently completing her BSc (Hons) in Psychology. She is working with Dr. Fawcett and Dr. Rash to determine the cognitive mechanisms involved in managing past and future painful events.",
+            "image": 'avatarMen',
+            width: "100%",
+            width2: "100%",
+            width3: "100%",
+            height: '100%'
+
+        },
+        {
+            "name": "Janaya Toope ",
+            "role": "Honours Student ",
+            "description": "Member of the Laboratory.",
+            "image": 'avatarMen',
+            width: "100%",
+            width2: "100%",
+            width3: "100%",
+            height: '100%'
+        },
+        {
             "name": "Cayley Thoms",
             "role": "Honours Student ",
             "description": "Member of the Laboratory.",
-            "image": 'chelsea',
+            "image": 'avatarMen',
             width: "100%",
             width2: "100%",
             width3: "100%",
@@ -111,7 +123,7 @@ let teamData = {
             "role": "Volunteer",
             "description": "Kathleen is a new volunteer in the Neuro Fog laboratory.",
             "image": 'avatarMen',
-            width: "65%",
+            width: "100%",
             width2: "100%",
             width3: "100%",
             height: '100%'
@@ -121,7 +133,7 @@ let teamData = {
             "role": "Volunteer",
             "description": "Iain is completing his BSc (Hons) in Behavioural Neuroscience with hopes to persue Medicine. He is currently working on his Honours project with Dr. Fawcett.",
             "image": 'avatarMen',
-            width: "65%",
+            width: "100%",
             width2: "100%",
             width3: "100%",
             height: '100%'
@@ -131,7 +143,7 @@ let teamData = {
             "role": "Volunteer",
             "description": "Iain is completing his BSc (Hons) in Behavioural Neuroscience with hopes to persue Medicine. He is currently working on his Honours project with Dr. Fawcett.",
             "image": 'avatarMen',
-            width: "65%",
+            width: "100%",
             width2: "100%",
             width3: "100%",
             height: '100%'
@@ -141,8 +153,8 @@ let teamData = {
             "name": "Vanessa Strong",
             "role": "Member",
             "description": "Liam is a 4th year hons student studying auditory working memory. From his love for music he became interested in how we percieve and understand music, leading to his current research.",
-            "image": 'liam',
-            width: "65%",
+            "image": 'avatarMen',
+            width: "100%",
             width2: "100%",
             width3: "100%",
         }
@@ -155,13 +167,11 @@ let teamData = {
             "role": "MUCEP Student",
             "description": "Zahin is completing his BSc in Computer Science and is currently working on developing the website for the laboratory",
             "image": 'zahin',
-            width: "65%",
+            width: "100%",
             width2: "100%",
             width3: "100%",
         }
     ]
-
-
 
 };
 
